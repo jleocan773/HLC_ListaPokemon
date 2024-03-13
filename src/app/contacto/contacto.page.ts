8ef894df5eb068b0ada29938a89bee0dd56a1acf
jleocan773 <115530217+jleocan773@users.noreply.github.com>
2024-03-13 18:37:39 +0100

    JLC | 2024-03-13 | Markers en el mapa

--- src/app/contacto/contacto.page.ts
+++ src/app/contacto/contacto.page.ts
@@ -28,12 +28,44 @@ export class ContactoPage implements OnInit {
   loadMap() {
     console.log('Cargando el mapa...');
 
-    let latitud = 36.6797047;
-    let longitud = -5.4470656;
     let zoom = 17;
-    this.map = L.map('mapId').setView([latitud, longitud], zoom);
+
+    let latEmpresaPrac = 36.67874440726268;
+    let lonEmpresaPrac = -5.450390155484502;
+    this.map = L.map('mapId').setView([latEmpresaPrac, lonEmpresaPrac], zoom);
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(
       this.map
     );
+
+    //Definiimos la ubicación del marcador para la empresa
+    let markerLocationEmpresaprac = L.latLng(latEmpresaPrac, lonEmpresaPrac);
+
+    //Añadimos el marcador al mapa
+    let marker1 = L.marker(markerLocationEmpresaprac, {
+      alt: 'Ubicación de la empresa',
+    }).addTo(this.map);
+
+    let latitudIes = 36.6797047;
+    let longitudIes = -5.4470656;
+    
+    // Definimos la ubicación del marcador
+    let markerLocationIes = L.latLng(latitudIes, longitudIes);
+
+    //Añadimos el marcador al mapa
+    let marker2 = L.marker(markerLocationIes, {
+      alt: 'Ubicación del instituto',
+    }).addTo(this.map);
+
+    //Hacemos un Popup para este marcador
+    marker1
+      .bindPopup(
+        '<b>I.Piñavall SL</b><br>A lo mejor termino haciendo las prácticas aquí.'
+      )
+      .openPopup();
+
+    //Hacemos un Popup para este marcador
+    marker2
+      .bindPopup('<b>I.E.S Los Remedios</b><br>Aquí he estudiado.')
+      .openPopup();
   }
 }
